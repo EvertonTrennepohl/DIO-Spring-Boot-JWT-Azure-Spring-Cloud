6966776d3754c2eeba02df6b2f8e7f4a738dbff2
EvertonTrennepohl <gauchotp@gmail.com>
2022-02-02 14:56:03 -0300

    Application divided into two modules, 'Spring Boot JWT' and 'Spring Boot Azure'.

---
+++ Spring-Boot-JWT/src/main/java/one/digitalinnovation/jwt/controller/UserController.java
@@ -0,0 +1,23 @@
+package one.digitalinnovation.jwt.controller;
+
+import one.digitalinnovation.jwt.data.UserData;
+import one.digitalinnovation.jwt.service.UserDetailsServiceImpl;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    private final UserDetailsServiceImpl userDetailsService;
+
+    public UserController(UserDetailsServiceImpl userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @RequestMapping("/all-users")
+    public List<UserData> listAllUsers() {
+        return userDetailsService.listUsers();
+    }
+}
---
+++ Spring-Boot-JWT/src/main/java/one/digitalinnovation/jwt/security/JWTAuthenticationFilter.java
@@ -0,0 +1,62 @@
+package one.digitalinnovation.jwt.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import one.digitalinnovation.jwt.data.UserData;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try {
+            UserData creds = new ObjectMapper()
+                    .readValue(request.getInputStream(), UserData.class);
+            return authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                    creds.getUserName(),
+                    creds.getPassword(),
+                    new ArrayList<>())
+            );
+        } catch (StreamReadException e) {
+            e.printStackTrace();
+        } catch (DatabindException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+                                            FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        String token = JWT.create()
+                .withSubject(((User) authResult.getPrincipal()).getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME))
+                .sign(Algorithm.HMAC512(SecurityConstants.SECRET.getBytes()));
+        response.addHeader(SecurityConstants.HEADER_STRING, SecurityConstants.TOKEN_PREFIX + token);
+    }
+}
